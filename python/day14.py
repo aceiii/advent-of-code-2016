3f44abb7059009ce13b5c18fca0cf198059d4646
Borin Ouch <borin.ouch@gmail.com>
2026-02-01 18:48:36 -0800

    Implement day14 part 1

---
+++ python/day14.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+from hashlib import md5
+
+
+def contains_three_in_row(s):
+    for i in range(0, len(s)-2):
+        if s[i] == s[i+1] and s[i] == s[i+2]:
+            return s[i]
+    return None
+
+
+def contains_five_in_row(s):
+    for i in range(0, len(s)-4):
+        if s[i] == s[i+1] and s[i] == s[i+2] and s[i] == s[i+3] and s[i] == s[i+4]:
+            return s[i]
+    return None
+
+
+def is_key(prefix, n):
+    key = prefix + str(n)
+    h = md5(key.encode('ascii')).hexdigest()
+    c = contains_three_in_row(h);
+    if c is None:
+        return False
+    for i in range(n+1, n+1001):
+        key2 = prefix + str(i)
+        h2 = md5(key2.encode('ascii')).hexdigest()
+        if contains_five_in_row(h2) == c:
+            return True
+    return False
+
+
+def part1(lines):
+    prefix = lines[0]
+    found = 0
+    target = 64
+    idx = 0
+    while True:
+        if is_key(prefix, idx):
+            found += 1
+            if found == target:
+                return idx
+        idx += 1
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    lines = sys.stdin.read().strip().split("\n")
+    print("Part1: {}".format(part1(lines)))
+    print("Part2: {}".format(part2(lines)))
+
+
+if __name__ == "__main__":
+    main()
+
